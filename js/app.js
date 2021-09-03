3289aa62c805ee3e0e19f551f4960f5af08b875e
JunayedAhmed <junayedahmed11099@gmail.com>
2021-09-03 15:10:03 +0600

    weather icon added

--- js/app.js
+++ js/app.js
@@ -1,13 +1,23 @@
 const API_key =`4c7214298230d0776d7c1b32e46a5e57`;
 const searchTemperature =()=>{
     const city = document.getElementById('city-name').value;
-    const url=`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`
+    const url=`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric`
     console.log(url);
     fetch(url)
     .then(res => res.json())
     .then(data => displayTemperature(data))
 }
 
+const setInnerText = (id,text) =>{
+    document.getElementById(id).innerText = text;
+}
+
 const displayTemperature = temperature =>{
-    console.log(temperature);
+    setInnerText('city',temperature.name);
+    setInnerText('temperature',temperature.main.temp);
+    setInnerText('condition',temperature.weather[0].main)
+    //set weather icon
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src',url);//setAttribute e first e ki use korbo and second e ki set korbo ta dibo.
 }
